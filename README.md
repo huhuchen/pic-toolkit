9debd02bdf4f8d7d6ae8ad1c36ff561e0e0369e3
huhuchen <chengengtiger@gmail.com>
2012-12-04 23:40:19 +0800

    init commit

--- README.md
+++ README.md
@@ -2,3 +2,32 @@ pic-toolkit
 ===========
 
 some scripts to deal with picture with PIL
+
+
+
+一些资料，先备记在这里：
+============================
+
+.. `PIL 官方文档`: http://www.pythonware.com/library/pil/handbook/index.htm
+
+.. `Lion下安装PIL1.1.7`：http://davidx.me/2011/12/11/lion%E4%B8%8B%E5%AE%89%E8%A3%85pil1-1-7/
+
+.. `how to install PIL on mac os x 10.7.2 Lion`：http://stackoverflow.com/questions/9070074/how-to-install-pil-on-mac-os-x-10-7-2-lion/11368029#11368029
+
+.. `Getting Photo Metadata (EXIF) Using Python`：http://www.blog.pythonlibrary.org/2010/03/28/getting-photo-metadata-exif-using-python/
+
+
+.. `gif图片拆帧及合成[python]实现`：http://blog.csdn.net/yangalbert/article/details/7603338
+
+
+.. `PIL(Python Image Library)生成验证码`：http://blog.csdn.net/marising/article/details/4004650
+
+.. `python图形处理库PIL(Python Image Library)`: http://blog.chinaunix.net/uid-9185047-id-445015.html
+
+.. `Python 使用 PIL 给图片添加水印`：http://liluo.org/blog/2011/08/python-add-watermark-with-pil/
+
+.. `Watermark with PIL (Python recipe) `: http://code.activestate.com/recipes/362879-watermark-with-pil/
+
+
+
+
---
+++ watermark.py
@@ -0,0 +1,115 @@
+#coding:utf-8
+"""
+watermark
+~~~~~~~~~~~
+使用python库PIL对图片添加水印，在安装PIL前需要先安装：
+libjpeg
+lzlib
+freetype
+"""
+
+import sys
+from cStringIO import StringIO
+import Image, ImageDraw, ImageFont
+from ExifTags import TAGS
+
+
+def picopen(image):
+    if not image:return
+
+    if hasattr(image, 'getim'): # a PIL Image object
+        im = image
+    else:
+        if not hasattr(image, 'read'): # image content string
+            image = StringIO(image)
+        try:
+            im = Image.open(image.read()) # file-like object
+        except IOError, e:
+            print e
+            return
+
+
+    if im.mode == 'RGBA':
+        p = Image.new('RGBA', im.size, 'white')
+        try:
+            x, y = im.size
+            p.paste(im, (0, 0, x, y), im)
+            im = p
+        except:
+            pass
+        del p
+
+    if im.mode == 'P':
+        need_rgb = True
+    elif im.mode == 'L':
+        need_rgb = True
+    elif im.mode == 'CMYK':
+        need_rgb = True
+    else:
+        need_rgb = False
+
+    if need_rgb:
+        im = im.convert('RGB', dither=Image.NONE)
+
+    return im
+
+
+def watermark_use_text(im, text, outfile, pos_x=0, pos_y=0, fonttype="", fontsize=60, fontcolor=(0,0,0)):
+    if not hasattr(im, 'getim'): # a PIL Image object
+        raise NotIsPilObject
+    watermark = Image.new("RGBA", (im.size[0], im.size[1]))
+
+    draw = ImageDraw.ImageDraw(watermark, "RGBA")
+
+    font = ImageFont.truetype(fonttype, fontsize)
+
+    if fontcolor:
+        draw.text((pos_x, pos_y), text, font=font, fill=fontcolor)
+    else:
+        draw.text((pos_x, pos_y), text, font=font)
+
+    im.paste(watermark, None, watermark)
+
+    im.save(outfile)
+
+
+def get_metadata(im):
+    ret = {}
+    if not hasattr(im, 'getim'): # a PIL Image object
+        raise NotIsPilObject
+    info = im._getexif()
+    for tag, value in info.items():
+        decoded = TAGS.get(tag, tag)
+        ret[decoded] = value
+    return ret
+
+class NotIsPilObject(Exception):
+    "not is a PIL Object"
+
+
+def handler(source_file, target_file, text=None):
+    fontcolor = (239, 238, 86)
+    fontsize = 90
+    fonttype="founder-simplified.ttf"
+    im = picopen(source_file)
+    if not text:
+        meta = get_metadata(im)
+        _date = meta.get("DateTime")
+        if _date:
+            text = _date.split(" ")[0].replace(":", "/")
+    text = " ".join(list(text))
+    pos_x = im.size[0] * 0.75
+    pos_y = im.size[1] * 0.8
+    watermark_use_text(im, text, target_file, pos_x=pos_x, pos_y=pos_y, fonttype=fonttype, fontsize=fontsize, fontcolor=fontcolor)
+
+
+if __name__ == "__main__":
+    argv = sys.argv
+    if len(argv) >= 3:
+        text = None
+        source_file = argv[1]
+        target_file = argv[2]
+        if len(argv) == 4:
+            text = argv[3]
+        handler(source_file, target_file, text)
+        handler(source_file, target_file, text)
